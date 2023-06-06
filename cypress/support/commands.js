a5965b81c348012780dcb6f96941d1ffcb0dc391
Anton.Stroich <qaas1988@gmail.com>
2023-06-06 14:14:47 +0400

    feat: add MailUserMenu.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,17 @@
 import LandingPage from "..//integration/PageObjects/LandingPage";
 import LoginToMailPage from "..//integration/PageObjects/LoginToMailPage";
+import MailNavigationBar from "../integration/PageObjects/MailNavigationBar";
+import MailUserMenu from "../integration/PageObjects/MailUserMenu";
 
-Cypress.Commands.add("logInToMail", (email, password) => { 
+Cypress.Commands.add("logInToMail", (email, password)=> { 
     const landingPage = new LandingPage();
     const loginToMailPage = new LoginToMailPage();
+    const mailNavigationBar = new MailNavigationBar();
+    const mailUserMenu = new MailUserMenu();
+    
     landingPage.openAndClickMailBtn();
     loginToMailPage.logInToMail(email, password);
+    mailNavigationBar.clickMessagesBtn();
+    mailNavigationBar.clickUserBtn();
+    mailUserMenu.clickLogOutBtn();
   })
